556335c5c6d5c7560218c752a42ed2319a0a4b5b
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-05-12 23:05:11 +0530

    Testing Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [calcInput,setCalcInput] = useState('');
+  const [ans,setAns] = useState('')
+  const btnValues = ['7','8','9','+','4','5','6','-','1','2','3','*','C','0','=','/']
+
+  const handleClick = (val) => {
+    if(val == 'C'){
+        setAns(prev => '')
+        setCalcInput(prev => '')
+      return
+    }
+    if(val == '='){
+      if(calcInput == ''){
+        setAns(prev => 'Error')
+      }else{
+        setAns(prev => window.eval(calcInput))
+      }
+      return 
+    }
+    setCalcInput(prev => prev + val);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <input type='text' value={calcInput}/> <br />
+      {ans && <div className='ans'>{ans}</div>}
+      <div className='btns'>
+        {btnValues.map(btn => {
+          return <button key={btn} type='button' onClick={e => handleClick(btn)}>{btn}</button>
+        })}
+      </div>
     </div>
   );
 }
